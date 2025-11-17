8a0261c4a56cb30fb67fc32ece48e4f909a5603a
rineluo920-svg <rineluo920@gmail.com>
2025-11-17 21:38:38 +1100

    User Registration

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path('register/', views.register, name='register'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -4,19 +4,33 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import UserRegistrationForm, EmailAuthenticationForm
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = EmailAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-            next_url = request.GET.get('next', reverse("chipin:home"))
-            return HttpResponseRedirect(next_url)
-        else:
-            messages.error(request, "Invalid Credentials.")
-    return render(request, "users/login.html")
+            return redirect('chipin:home')
+    else:
+        form = EmailAuthenticationForm(request)
+    return render(request, 'users/login.html', {'form': form})
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You can now log in.")
+            return redirect('users:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required(login_url='users:login')
+def user(request):
+    return render(request, "chipin/home.html")
 
 def logout_view(request):
     logout(request)
