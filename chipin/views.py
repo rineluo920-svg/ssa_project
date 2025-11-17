4a67a93b8bd3de2b5e66f71fe9732af96c9e0173
rineluo920-svg <rineluo920@gmail.com>
2025-11-17 21:25:48 +1100

    Initial commit

---
+++ chipin/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='users:login')
+def home(request):
+    return render(request, "chipin/home.html")
